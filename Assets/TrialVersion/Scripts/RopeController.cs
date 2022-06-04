ef5ba485f9c29803ae44702665bd7ebbe9555546
Sevde Sarıkaya <sevdesarikaya13@gmail.com>
2022-06-04 03:29:23 +0300

    rope swing

---
+++ Assets/TrialVersion/Scripts/RopeController.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//If we have a stiff rope, such as a metal wire, then we need a simplified solution
+//this is also an accurate solution because a metal wire is not swinging as much as a rope made of a lighter material
+public class RopeController : MonoBehaviour 
+{
+    //Objects that will interact with the rope
+    public Transform gameObject;
+    private bool isGoingLeft;
+
+    void Start()
+    {
+	    StartCoroutine(nameof(Swing));
+    }
+
+
+	void Update()
+	{
+		var dist = (gameObject.transform.position - transform.position).magnitude;
+		if (dist < transform.localScale.y / 2f)
+		{
+		}
+
+	}
+
+	IEnumerator Swing()
+	{
+
+		var duration = 5f;
+		var elapsed = 1f;
+		while (true)
+		{
+			elapsed += Time.deltaTime;
+			var ratio = elapsed / duration;
+			if (ratio >= 1f)
+			{
+				elapsed = Time.deltaTime;
+				ratio = elapsed / duration;
+			}
+
+			var z = 0f;
+			if (ratio <= 0.5f)
+			{
+				z = Mathf.Lerp(-60f, 60, 2f*ratio);
+				transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, z);
+			}
+			else
+			{
+				z = Mathf.Lerp(60f, -60f, 2f*(ratio - 0.5f));
+				transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, z);
+			}
+			
+			yield return null;
+		}
+	}
+	
+}
